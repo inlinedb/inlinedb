90bf140c18db30be0fa46bee186740851fcc4784
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-05-23 20:22:19 -0500

    handle insert rows query

--- src/query.js
+++ src/query.js
@@ -1,10 +1,45 @@
+const getNextId = lastInsertId => lastInsertId + 1;
+
+const insertRows = (data, query) => {
+
+  let {lastInsertId} = query;
+
+  const rows = data.rows.concat(
+    query.rows.map(row => {
+
+      lastInsertId = getNextId(lastInsertId);
+
+      return Object.assign({$idbID: lastInsertId}, row);
+
+    })
+  );
+  const index = rows.reduce(
+    (indices, row, iterator) =>
+      Object.assign(indices, {[row.$idbID]: iterator}),
+    {}
+  );
+
+  return {
+    index,
+    rows
+  };
+
+};
+
 const types = {
   DELETE: 'DELETE',
   INSERT: 'INSERT',
   UPDATE: 'UPDATE'
 };
 
-const run = (queries, data) => data;
+const queryHandlers = {
+  [types.INSERT]: insertRows
+};
+
+const run = (queries, data) => queries.reduce(
+  (intermediateData, query) => queryHandlers[query.type](data, query),
+  data
+);
 
 module.exports = {
   run,
--- test/query.spec.js
+++ test/query.spec.js
@@ -22,4 +22,84 @@ describe('query', () => {
 
   });
 
+  describe('when inserting rows', () => {
+
+    const row = {column: 'column'};
+    const insertQuery = {
+      lastInsertId: 0,
+      rows: [row],
+      type: query.types.INSERT
+    };
+    let data,
+      updatedData;
+
+    beforeEach(() => {
+
+      data = {
+        index: {},
+        rows: []
+      };
+
+      updatedData = query.run([insertQuery], data);
+
+    });
+
+    it('should return data with inserted rows', () => {
+
+      const expectedRows = [
+        Object.assign({$idbID: 1}, row)
+      ];
+
+      expect(updatedData.rows).equals(expectedRows);
+
+    });
+
+    it('should build index for inserted rows', () => {
+
+      const expectedIndex = {
+        1: 0
+      };
+
+      expect(updatedData.index).equals(expectedIndex);
+
+    });
+
+    describe('and more rows', () => {
+
+      const insertMoreQuery = {
+        lastInsertId: 1,
+        rows: [row, row],
+        type: query.types.INSERT
+      };
+
+      beforeEach(() => updatedData = query.run([insertMoreQuery], updatedData));
+
+      it('should return data with inserted rows', () => {
+
+        const expectedRows = [
+          Object.assign({$idbID: 1}, row),
+          Object.assign({$idbID: 2}, row),
+          Object.assign({$idbID: 3}, row)
+        ];
+
+        expect(updatedData.rows).equals(expectedRows);
+
+      });
+
+      it('should build index for inserted rows', () => {
+
+        const expectedIndex = {
+          1: 0,
+          2: 1,
+          3: 2
+        };
+
+        expect(updatedData.index).equals(expectedIndex);
+
+      });
+
+    });
+
+  });
+
 });
